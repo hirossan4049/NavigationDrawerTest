2a924f7abc975ead3b6f39c860e2f0976bfbaa3e
hirossan4049 <haruto405329@gmail.com>
2020-03-04 21:11:00 +0900

    Initial commit

---
+++ app/src/main/java/com/example/navigationdrwwwww/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.navigationdrwwwww;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
